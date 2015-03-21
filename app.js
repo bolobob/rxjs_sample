c3efe71bedc6ef87251499500a03f0b25e663142
bolobob <aokishinichi@gmail.com>
2015-03-21 17:07:29 +0900

    sample: filerメソッド

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Rx.Observable.from([1, 2, 3])
-             .map(function(n) { return 2 * n; })
+             .filter(function(n) { return n % 2 === 1; })
              .subscribe(function(n) { console.log(n); });
