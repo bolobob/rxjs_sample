a574f05041da9419b2abae280d24b67ca6006daa
bolobob <aokishinichi@gmail.com>
2015-03-21 17:52:37 +0900

    sample: mergeメソッド

--- app.js
+++ app.js
@@ -5,3 +5,11 @@ Rx.Observable.from([1, 2, 3])
 Rx.Observable.fromEvent(document, 'click')
              .throttle(250)
              .subscribe(function(n) { console.log(n); });
+
+// merge
+var stream1 = Rx.Observable.from([1, 2, 3, 4, 5]);
+var stream2 = Rx.Observable.from([10, 20, 30]);
+Rx.Observable.merge(stream1, stream2)
+             .subscribe(function(n) { console.log(n); });
+
+
