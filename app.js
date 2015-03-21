f40931cbc1e0a8383621deccde18742d3718d5f3
bolobob <aokishinichi@gmail.com>
2015-03-21 17:47:51 +0900

    sample: throttleメソッド

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 Rx.Observable.from([1, 2, 3])
              .filter(function(n) { return n % 2 === 1; })
              .subscribe(function(n) { console.log(n); });
+
+Rx.Observable.fromEvent(document, 'click')
+             .throttle(250)
+             .subscribe(function(n) { console.log(n); });
