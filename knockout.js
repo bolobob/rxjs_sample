dc6fc6780bd720d219c4de9439193e89ca0b23f1
bolobob <aokishinichi@gmail.com>
2015-03-25 21:42:03 +0900

    sample: 複数対応

--- knockout.js
+++ knockout.js
@@ -4,6 +4,7 @@ function ViewModel() {
 
 function observable() {
   var value;
+  var subscribers = [];
   var observable = function(newValue) {
     if (typeof(newValue) === 'undefined') {
       return value;
@@ -13,16 +14,46 @@ function observable() {
     }
   };
   observable.valueChanged = function() {
-    observable['subscriber'](value);
+    subscribers.forEach(function(subscriber) {
+      subscriber(value);
+    });
+  }
+  observable.addSubscriber = function(subscriber) {
+    subscribers.push(subscriber);
   }
 
   return observable;
 }
 
 function bindViewModel(viewModel) {
+  var props = Object.getOwnPropertyNames(viewModel);
+  props.forEach(function(prop) {
+    var views = document.querySelectorAll('[data-bind=' + prop + ']'),
+        prop = viewModel[prop],
+        i;
 
+    for (i = 0; i < views.length; i++) {
+      prop.addSubscriber((function(view) {
+                            return function(newValue) {
+                              if (view.hasOwnProperty('value')) {
+                                view.value = newValue;
+                              } else {
+                                view.innerText = newValue;
+                              }
+                            };
+                          })(views[i]));
+    }
+
+    for (i = 0; i < views.length; i++) {
+      views[i].addEventListener('input', function(e) {
+        prop(e.target.value);
+      });
+    }
+  });
 }
 
-var viewModel = new ViewModel();
-bindViewModel(viewModel);
-viewModel.email('aokishinichi@gmail.com');
+$(function() {
+  var viewModel = new ViewModel();
+  bindViewModel(viewModel);
+  viewModel.email('aokishinichi@gmail.com');
+});
